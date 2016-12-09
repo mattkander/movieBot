14124d13ff01570a26e7a0da241e1458905b3383
matt <matthew.kiernander@healthlink.net>
2016-12-09 14:34:43 +1300

    Added post tweet and get tweet functions

--- movieBot.js
+++ movieBot.js
@@ -1,4 +1,22 @@
 /**
  * Created by MatthewK on 12/9/2016.
  */
-console.log("i am alive")
+console.log("movieBot is starting")
+
+//Imports the package
+var Twit = require('twit');
+
+//isolates keys in a separate file. Pulls in through an export. Creates a config object.
+var config = require("./config");
+//prints out all available info within the config object.
+//console.log(config);
+
+var T = new Twit(config);
+
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+    console.log(data)
+})
+
+T.get('followers/ids', { screen_name: 'badseedtech' },  function (err, data, response) {
+    console.log(data)
+})
