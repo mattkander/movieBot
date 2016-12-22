5b13e0ef6b964f10eb4834c5f391674afde307ee
matt <matthew.kiernander@healthlink.net>
2016-12-23 11:14:32 +1300

    Added streaming functionality.

--- movieBot.js
+++ movieBot.js
@@ -13,10 +13,53 @@ var config = require("./config");
 
 var T = new Twit(config);
 
-T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-    console.log(data)
-})
+//opens the stream
+var stream = T.stream('user');
+//anytime someone follows me
+stream.on('follow', followed);
 
-T.get('followers/ids', { screen_name: 'badseedtech' },  function (err, data, response) {
-    console.log(data)
-})
+function followed(eventMsg){
+    console.log('Follow event');
+    var name = eventMsg.source.name;
+    console.log("Name field is: "+name);
+    var screenName = eventMsg.source.screen_name;
+    tweet('@'+screenName+' you followed a work in progress');
+
+}
+
+/*
+
+The below sets an interval to tweet out every 20 seconds.
+
+setInterval(tweet, 1000*20)
+
+ var r = Math.floor(Math.random()*100);
+
+ var tweetContent = {status: 'Troy Smells '+r+"% worse than yesterday"};
+ */
+
+function tweet(txt) {
+    T.post('statuses/update', {status: txt}, function (err, data, response) {
+        console.log(txt);
+        console.log("Tweet out")
+    })
+}
+
+function tweeted(err, data, response){
+    if (err){
+        console.log("Something went wrong!");
+
+    }else{
+        console.log("Great Success");
+    }
+}
+
+
+
+function getFollowers() {
+    T.get('followers/ids', {screen_name: 'badseedtech'}, function (err, data, response) {
+        console.log(data)
+    })
+}
+
+
